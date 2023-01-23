8ddae02da8b1c4fa645ad314765c419e343da105
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-23 14:11:44 +0530

    initiate the application

---
+++ src/main/java/com/example/SpringDataMongoDB/SpringDataMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongoDbApplication.class, args);
+	}
+
+}
